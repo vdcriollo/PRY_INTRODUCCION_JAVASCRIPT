a63e44455506b130c0c856a0a2270d9380ed3406
vdcriollo <vdcriollo@espe.edu.ec>
2025-06-16 12:14:29 -0500

    saludo con boton

---
+++ public/js/saludo.js
@@ -0,0 +1,6 @@
+function saludar(){
+    let saludo = document.getElementById("mostrar_saludo");
+    saludo.innerHTML="<i>SALUDANDO DESDE EL BOTON MEDIANTE CLICK</i>";
+    console.log(saludo)
+
+}
